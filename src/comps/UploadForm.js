171d8cfa2ecc51526aff41fb94ef8fdfbe7cf022
Piero <piero.onorati@virgilio.it>
2021-08-13 20:05:22 +0200

    upload form

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,38 +1,35 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const UploadForm = () => {
-    
-    const { file, setFile } = useState(null);
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
 
-    const { error, setError } = useState(null);
+  const types = ['image/png', 'image/jpeg'];
 
-    // create an array with allowed image extensions
-    const types= ['image/png', 'image/jpg', 'image/jpeg'];
+  const handleChange = (e) => {
+    let selected = e.target.files[0];
 
-    const changeHandler = (e) =>{
-
-        // selected the image
-        let selected = e.target.files[0];
-
-        if (selected) {
-            setFile(selected);
-            setError('');
-        }else{
-            setFile(null);
-            setError('Please select an image file (png, jpg or jpeg)');
-        }
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError('');
+    } else {
+      setFile(null);
+      setError('Please select an image file (png or jpg)');
     }
+  };
 
-
-    return ( 
-        <form action="">
-            <input type="file" onChange = { changeHandler } />
-            <div className="output">
-                { error && <div className="error">{ error }</div>}
-                { file && <div>{ file.name }</div>}
-            </div>
-        </form>
-    );
+  return (
+    <form>
+      <label>
+        <input type="file" onChange={handleChange} />
+        <span>+</span>
+      </label>
+      <div className="output">
+        { error && <div className="error">{ error }</div>}
+        { file && <div>{ file.name }</div> }
+      </div>
+    </form>
+  );
 }
- 
+
 export default UploadForm;
